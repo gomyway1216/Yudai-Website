91e1754e9361ba2be0c5ec17f40ac789a3de1c79
Yudai Yaguchi <yyaguchi@wisc.edu>
2019-12-24 08:51:17 -0600

    set up

---
+++ src/pages/404.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const error = () => {
+  return <div>hello from error page</div>
+}
+
+export default error
---
+++ src/pages/content.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const content = () => {
+  return <div>hellor from contact</div>
+}
+
+export default content

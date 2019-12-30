66a3320465062346b652841f33f75da7bbdaf09c
Yudai Yaguchi <yyaguchi@wisc.edu>
2019-12-31 08:54:43 +0900

    installed packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,22 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: 'BackRoads',
+    description:
+      'Explore awesome worldwide tours & discover what makes each of them unique. Forget your daily routine & say yes to adventure',
+    author: '@yudai',
+  },
   /* Your site config here */
-  plugins: [`gatsby-plugin-sass`, `gatsby-plugin-styled-components`],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-styled-components`,
+  ],
 }
